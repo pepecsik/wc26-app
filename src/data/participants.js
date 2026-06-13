3f867d9b2968a77cf189702779171321b4561873
Winston <winston@Winstons-MacBook-Pro.local>
2026-06-13 15:22:06 +0100

    Add participant avatars

--- src/data/participants.js
+++ src/data/participants.js
@@ -8,30 +8,30 @@ const COLORS = [
 ];
 
 const raw = [
-  { name: 'Adam',                teams: ['JOR','USA'] },
-  { name: 'Ali',                 teams: ['UZB','COL'] },
-  { name: 'Blake',               teams: ['SWE','CRO'] },
-  { name: 'Charlie',             teams: ['NOR','GER'] },
-  { name: 'Chonga',              teams: ['CZE','SKO'] },
-  { name: 'Emma',                teams: ['CIV','ENG'] },
-  { name: 'J$',                  teams: ['PAN','TUR'] },
-  { name: 'Jimmy',               teams: ['ALG','SEN'] },
-  { name: 'Kimbo',               teams: ['CPV','MOR'] },
-  { name: 'Malou',               teams: ['CUR','URU'] },
-  { name: 'Michael',             teams: ['SCO','ECU'] },
-  { name: 'Nathanial',           teams: ['HAI','BEL'] },
-  { name: 'Pepe',                teams: ['SAF','JPN'] },
-  { name: 'Purcy',               teams: ['CAN','NED'] },
-  { name: 'Russ',                teams: ['COD','IRN'] },
-  { name: 'Rand',                teams: ['TUN','BRA'] },
-  { name: 'Rogier',              teams: ['QAT','AUT'] },
-  { name: 'Sabo',                teams: ['BOS','MEX'] },
-  { name: 'Scotty2Hotty',        teams: ['GHA','POR'] },
-  { name: 'Sebastian',           teams: ['PAR','FRA'] },
-  { name: 'Sjaak',               teams: ['SAU','ESP'] },
-  { name: 'Tim',                 teams: ['NZL','SWI'] },
-  { name: 'Tobias',              teams: ['EGY','ARG'] },
-  { name: 'Will Hunt',           teams: ['IRQ','AUS'] },
+  { name: 'Adam',         teams: ['JOR','USA'], photo: '/avatars/Adam.png' },
+  { name: 'Ali',          teams: ['UZB','COL'], photo: '/avatars/Ali .png' },
+  { name: 'Blake',        teams: ['SWE','CRO'], photo: '/avatars/Blake.png' },
+  { name: 'Charlie',      teams: ['NOR','GER'], photo: '/avatars/Charlie.png' },
+  { name: 'Chonga',       teams: ['CZE','SKO'], photo: '/avatars/Chonga.png' },
+  { name: 'Emma',         teams: ['CIV','ENG'], photo: '/avatars/Emma.png' },
+  { name: 'J$',           teams: ['PAN','TUR'], photo: '/avatars/J$.png' },
+  { name: 'Jimmy',        teams: ['ALG','SEN'], photo: '/avatars/Jimmy.png' },
+  { name: 'Kimbo',        teams: ['CPV','MOR'], photo: '/avatars/Kimbo.png' },
+  { name: 'Malou',        teams: ['CUR','URU'], photo: '/avatars/Malou.png' },
+  { name: 'Michael',      teams: ['SCO','ECU'], photo: '/avatars/Michael.png' },
+  { name: 'Nathanial',    teams: ['HAI','BEL'], photo: '/avatars/Nathanial.png' },
+  { name: 'Pepe',         teams: ['SAF','JPN'], photo: '/avatars/Pepe.png' },
+  { name: 'Purcy',        teams: ['CAN','NED'], photo: '/avatars/Purcy.png' },
+  { name: 'Russ',         teams: ['COD','IRN'], photo: '/avatars/Russ.png' },
+  { name: 'Rand',         teams: ['TUN','BRA'], photo: '/avatars/Rand.png' },
+  { name: 'Rogier',       teams: ['QAT','AUT'], photo: '/avatars/Rogier.png' },
+  { name: 'Sabo',         teams: ['BOS','MEX'], photo: '/avatars/Sabo.png' },
+  { name: 'Scotty2Hotty', teams: ['GHA','POR'], photo: '/avatars/Scotty2Hotty.png' },
+  { name: 'Sebastian',    teams: ['PAR','FRA'], photo: '/avatars/Sebastian SS Sparenberg.png' },
+  { name: 'Sjaak',        teams: ['SAU','ESP'], photo: '/avatars/Sjaak.png' },
+  { name: 'Tim',          teams: ['NZL','SWI'], photo: '/avatars/Tim.png' },
+  { name: 'Tobias',       teams: ['EGY','ARG'], photo: '/avatars/Tobias.png' },
+  { name: 'Will Hunt',    teams: ['IRQ','AUS'], photo: '/avatars/Will Hunt.png' },
 ];
 
 // Build lookup maps
@@ -39,7 +39,6 @@ export const PARTICIPANTS = raw.map((p, i) => ({
   ...p,
   color: COLORS[i % COLORS.length],
   initials: p.name.split(/\s+/).map(w => w[0].toUpperCase()).join('').slice(0, 2),
-  photo: null, // swap to '/avatars/name.jpg' when photos are available
 }));
 
 // team code → participant
